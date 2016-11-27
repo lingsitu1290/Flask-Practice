ea2b66d5b0e63622868f07c88222f1c891ccc185
Ling <lingsitu1290@gmail.com>
2016-11-27 00:19:25 +0000

    Set upusers table

--- routes.py
+++ routes.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from models import db 
 
 app = Flask(__name__)
+
+app.config('SQLALCHEMY_DATABASE_URI') = 'postgresql://localhost/learningflask'
+db.init_app(app)
 
 @app.route("/")
 def index():
@@ -9,7 +13,7 @@ def index():
 @app.route("/about")
 def about():
   return render_template("about.html")
-  
+
 if __name__=="__main__":
     app.debug = True
     app.run(host="0.0.0.0")
